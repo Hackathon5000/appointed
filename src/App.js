aa273749b20cc89f10a8c4ad3fb5996dc3f309c6
fabiolamariapina <fabiolamariapina@gmail.com>
2021-02-10 12:07:58 -0500

    created basic form with first name, last name, and email

--- src/App.js
+++ src/App.js
@@ -1,44 +1,55 @@
-import React, { useState } from 'react';
-import { Route, Switch } from 'react-router-dom';
-import './App.css';
+import React, { useState } from "react";
+import { Route, Switch } from "react-router-dom";
+import "./App.css";
 
-import Home from './components/home/home';
-import Information from './components/information/information';
-import DistanceInfo from './components/distanceInfo/distanceInfo';
-import MaskInfo from './components/maskInfo/maskInfo';
-import Nav from './components/Nav/nav';
-import About from './components/about/about';
-
+import Home from "./components/home/home";
+import Information from "./components/information/information";
+import DistanceInfo from "./components/distanceInfo/distanceInfo";
+import MaskInfo from "./components/maskInfo/maskInfo";
+import Nav from "./components/Nav/nav";
+import About from "./components/about/about";
+import { Form, Input } from "semantic-ui-react";
 
 function App() {
   return (
-		<div className='App'>
-			<header>
-				<Nav></Nav>
-			</header>
-			<main>
-				Hello from main
-				<Switch>
-					<Route exact path='/'>
-						<Home />
-					</Route>
-					<Route exact path='/information'>
-						<Information />
-					</Route>
-					<Route exact path='/about'>
-						<About/>
-					</Route>
-					<Route>
-						<DistanceInfo exact path='/distanceInfo' />
-					</Route>
-					<Route exact path='/maskinfo'>
-						<MaskInfo />
-					</Route>
-				</Switch>
-			</main>
-			<footer>Hello from footer</footer>
-		</div>
-	);
+    <div className="App">
+      <header>
+        <Nav></Nav>
+      </header>
+      <main>
+        Hello from main
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/information">
+            <Information />
+          </Route>
+          <Route exact path="/about">
+            <About />
+          </Route>
+          <Route>
+            <DistanceInfo exact path="/distanceInfo" />
+          </Route>
+          <Route exact path="/maskinfo">
+            <MaskInfo />
+          </Route>
+        </Switch>
+        <Form>
+          <Form.Field required inline>
+            <label> First Name </label> <Input placeholder="Legal Name" />
+          </Form.Field>{" "}
+          <Form.Field required inline>
+            <label> Last Name </label> <Input placeholder="Legal Name" />
+          </Form.Field>{" "}
+          <Form.Field required inline>
+            <label> Email </label> <Input placeholder="Legal Name" />
+          </Form.Field>{" "}
+        </Form>
+      </main>
+      <footer>Hello from footer</footer>
+    </div>
+  );
 }
 
 export default App;
