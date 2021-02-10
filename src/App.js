b0e29773c139f5051e472ab20940c8e6ab9348d5
datacreator001 <amirrorphone@gmail.com>
2021-02-10 10:59:55 -0500

    homePage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import { useState } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { Distanceinfo,Homepage,Information,Maskinfo} from './components'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import {
+	Distanceinfo,
+	Homepage,
+	information,
+	Information,
+	Maskinfo,
+} from './components';
 
 const App = () => {
-  return (
-    <div>
-      <Homepage/>
-    </div>
-  )
-}
+	return (
+		<div>
+			<header></header>
+			<main>
+				<Homepage />
+				<Switch>
+					<Route exact path='/information'>
+						<Information/>
+					</Route>
+				</Switch>
+			</main>
+			<footer></footer>
+		</div>
+	);
+};
 
-export default App
+export default App;
--- src/components/HomePage/Homepage.jsx
+++ src/components/HomePage/Homepage.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Link, BrowserRouter as Route, Switch} from 'react-router-dom';
 
 const Homepage = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+	return (
+		<div>
+			<h1>Welcome to Miami Vaxx</h1>
+			<div style={{ display: 'flex', justifyContent: 'space-between' }}>
+				<h2>
+					Click on the information tab to find out how you can get tested for
+					Covid 19
+				</h2>
+			</div>
+			<div style={{ height: '30px', background: 'gray' }}>
+				<Link to={'../Information'}>Information</Link>
+				<Switch>
+					
+					
+					
+				</Switch>
+			</div>
+		</div>
+	);
+};
 
-export default Homepage
+export default Homepage;
