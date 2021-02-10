c258afb58b36cadc4a4089bb6571c61fef8eaf33
fabiolamariapina <fabiolamariapina@gmail.com>
2021-02-10 12:07:58 -0500

    created basic form with first name, last name, and email

--- src/components/VaccineForm.js
+++ src/components/VaccineForm.js
@@ -1,15 +1,23 @@
-import React, { Component } from 'react'
-import { Form } from 'semantic-ui-css'
+import React, { Component } from "react";
+import { Form, Input } from "semantic-ui-css";
 
 export default class VaccineForm extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Schedule your vaccine</h1>
-                <Form>
-                    
-                </Form>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <Form>
+          <Form.Field required inline>
+            <label> First Name </label> <Input placeholder="Legal Name" />
+          </Form.Field>{" "}
+          <Form.Field required inline>
+            <label> Last Name </label> <Input placeholder="Legal Name" />
+          </Form.Field>{" "}
+          <Form.Field required inline>
+            <label> Email </label> <Input placeholder="Legal Name" />
+          </Form.Field>{" "}
+
+        </Form>
+      </div>
+    );
+  }
 }
