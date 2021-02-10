5f52b7ff213addb11858d1a80ca7e34f9bc4fdb4
fabiolamariapina <fabiolamariapina@gmail.com>
2021-02-10 13:38:40 -0500

    claned up router code in App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Route, Switch } from "react-router-dom";
 import "./App.css";
-
+// components
 import Home from "./components/home/home";
 import Information from "./components/information/information";
 import DistanceInfo from "./components/distanceInfo/distanceInfo";
@@ -19,27 +19,12 @@ function App() {
       <main>
         Hello from main
         <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route exact path="/information">
-            <Information />
-          </Route>
-          <Route exact path="/about">
-            <About />
-          </Route>
-          <Route>
-            <DistanceInfo exact path="/distanceInfo" />
-          </Route>
-          <Route exact path="/maskinfo">
-            <MaskInfo />
-          </Route>
-          <Route exact path="/maskinfo">
-            <MaskInfo />
-          </Route>
-          <Route exact path="/vaccineform">
-            <VaccineForm />
-          </Route>
+          <Route path="/" exactComponent={Home} />
+          <Route path="/information" exactComponent={Information} />
+          <Route path="/about" exactComponent={About} />
+          <Route path="/distanceInfo" exactComponent={DistanceInfo} />
+          <Route path="/maskinfo" exactComponent={MaskInfo} />
+          <Route path="/vaccineform" exactComponent={VaccineForm} />
         </Switch>
       </main>
       <footer>Hello from footer</footer>
