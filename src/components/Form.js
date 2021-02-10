3702ca8f27d4f64e53b840d744c5ad6e8fcd7698
fabiolamariapina <fabiolamariapina@gmail.com>
2021-02-10 10:24:10 -0500

    created form component

---
+++ src/components/Form.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Form extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Schedule your vaccine</h1>
+            </div>
+        )
+    }
+}
