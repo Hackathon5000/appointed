695898bbd9d5680545930ee6c5520cd636d8b8d7
fabiolamariapina <fabiolamariapina@gmail.com>
2021-02-10 10:56:54 -0500

    imported form from semantic ui and changed file name

--- src/components/VaccineForm.js
+++ src/components/VaccineForm.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
+import { Form } from 'semantic-ui-css'
 
-export default class Form extends Component {
+export default class VaccineForm extends Component {
     render() {
         return (
             <div>
                 <h1>Schedule your vaccine</h1>
+                <Form>
+                    
+                </Form>
             </div>
         )
     }
