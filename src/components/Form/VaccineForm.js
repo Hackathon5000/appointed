8358a895f553fd7e364d82e8b0ba4f090704752e
fabiolamariapina <fabiolamariapina@gmail.com>
2021-02-10 14:24:26 -0500

    added error is user does not enter a field

--- src/components/Form/VaccineForm.js
+++ src/components/Form/VaccineForm.js
@@ -1,34 +1,43 @@
 import React, { Component } from "react";
 import { Form, Input } from "semantic-ui-react";
 import DatePicker from "react-datepicker";
-import styled from "styled-components";
+// import styled from "styled-components";
 
 export default class VaccineForm extends Component {
   render() {
-    const StyledInput = styled(Input)`
-      width: 50%;
-    `;
-    const StyledDatePicker = styled(DatePicker)`
-      width: 50%;
-    `;
+    // const StyledInput = styled(Input)`
+    //   width: 50%;
+    // `;
+    // const StyledDatePicker = styled(DatePicker)`
+    //   width: 50%;
+    // `;
     return (
       <div>
+        <h1>Testing</h1>
         <Form>
           <Form.Field required>
-            <label> First Name </label> <StyledInput placeholder="Legal Name" />
+            <label> Full Name </label>{" "}
+            <Input
+              error="Please enter your full name"
+              fluid
+              placeholder="Full Name"
+            />
           </Form.Field>{" "}
           <Form.Field required>
-            <label> Last Name </label> <StyledInput placeholder="Legal Name" />
-          </Form.Field>{" "}
-          <Form.Field required>
-            <label> Email </label> <StyledInput placeholder="Email" />
+            <label> Email </label>{" "}
+            <Input error="Please enter your email" fluid placeholder="Email" />
           </Form.Field>{" "}
           <Form.Field required>
             <label> Phone Number </label>{" "}
-            <StyledInput placeholder="Phone Number" />
+            <Input
+              error="Please enter your phone number"
+              fluid
+              placeholder="Phone Number"
+            />
           </Form.Field>{" "}
           <Form.Field required>
-            <label> Date of Birth </label> <StyledDatePicker />
+            <label> Date of Birth </label>{" "}
+            <DatePicker error="Please enter your date of birth" fluid />
           </Form.Field>{" "}
         </Form>{" "}
       </div>
