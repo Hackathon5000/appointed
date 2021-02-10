213a735e641317f093d5992fa9e3320d851e64df
fabiolamariapina <fabiolamariapina@gmail.com>
2021-02-10 14:24:26 -0500

    added error is user does not enter a field

--- src/App.js
+++ src/App.js
@@ -1,30 +1,45 @@
 import React, { useState } from "react";
 import { Route, Switch } from "react-router-dom";
 import "./App.css";
-// components
+
 import Home from "./components/home/home";
 import Information from "./components/information/information";
 import DistanceInfo from "./components/distanceInfo/distanceInfo";
 import MaskInfo from "./components/maskInfo/maskInfo";
 import Nav from "./components/Nav/nav";
 import About from "./components/about/about";
-import VaccineForm from "./components/Form/VaccineForm";
+import VaccineForm from "./components/form/vaccineForm.js";
 
 function App() {
   return (
     <div className="App">
       <header>
-        <Nav></Nav>
+        <Nav />
       </header>
       <main>
         Hello from main
         <Switch>
-          <Route path="/" exactComponent={Home} />
-          <Route path="/information" exactComponent={Information} />
-          <Route path="/about" exactComponent={About} />
-          <Route path="/distanceInfo" exactComponent={DistanceInfo} />
-          <Route path="/maskinfo" exactComponent={MaskInfo} />
-          <Route path="/vaccineform" exactComponent={VaccineForm} />
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/information">
+            <Information />
+          </Route>
+          <Route exact path="/about">
+            <About />
+          </Route>
+          <Route exact path="/distanceinfo">
+            <DistanceInfo />
+          </Route>
+          <Route exact path="/maskinfo">
+            <MaskInfo />
+          </Route>
+          <Route exact path="/maskinfo">
+            <MaskInfo />
+          </Route>
+          <Route exact path="/vaccineform">
+            <VaccineForm />
+          </Route>
         </Switch>
       </main>
       <footer>Hello from footer</footer>
