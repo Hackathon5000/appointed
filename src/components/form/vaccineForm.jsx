2f0d172700f5b06571cbccb600b466aeb7f93b6f
fabiolamariapina <fabiolamariapina@gmail.com>
2021-02-11 12:49:59 -0500

    added form functionality using hooks

--- src/components/form/vaccineForm.jsx
+++ src/components/form/vaccineForm.jsx
@@ -4,7 +4,14 @@ import DatePicker from "react-datepicker";
 import styled from "styled-components";
 
 export default function VaccineForm() {
+  const [firstName, setFirstName] = useState(" ");
+  const [lastName, setLastName] = useState(" ");
+  const [email, setEmail] = useState(" ");
+  const [phoneNumber, setPhoneNumber] = useState(" ");
   const [selectedDate, setSelectedDate] = useState(null);
+  const [checked, setChecked] = useState(false);
+  const handleClick = () => setChecked(!checked);
+  const [zipCode, setZipCode] = useState(" ");
   const ParentDiv = styled.div`
     display: flex;
     justify-content: center;
@@ -22,9 +29,30 @@ export default function VaccineForm() {
       <h1>Pre-Register Now For The COVID-19 Vaccine</h1>
       <ParentDiv>
         <Form>
-          <Form.Input label="Name" placeholder="Enter your full legal name" />
-          <Form.Input label="Email" placeholder="example@example.com" />
-          <Form.Input label="Phone Number" placeholder="1234567890" />
+          <Form.Input
+            label="First Name"
+            placeholder="First Name"
+            value={firstName}
+            onChange={(event) => setFirstName(event.target.value)}
+          />
+          <Form.Input
+            label="Last Name"
+            placeholder="Last Name"
+            value={lastName}
+            onChange={(event) => setLastName(event.target.value)}
+          />
+          <Form.Input
+            label="Email"
+            placeholder="example@example.com"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
+          <Form.Input
+            label="Phone Number"
+            placeholder="1234567890"
+            value={phoneNumber}
+            onChange={(event) => setPhoneNumber(event.target.value)}
+          />
           <Form.Field>
             <label> Date of Birth </label>{" "}
             <DatePicker
@@ -32,12 +60,37 @@ export default function VaccineForm() {
               onChange={(date) => setSelectedDate(date)}
             />
           </Form.Field>{" "}
-          <Form.Checkbox inline label="Florida Resident" />
-          <Form.Checkbox inline label="Current Epi-Pen User" />
-          <Form.Checkbox inline label="Front Line Medical Worker" />
-          <Form.Checkbox inline label="Are you seeking your first dose?" />
-          <Form.Input label="Zip Code" placeholder="Enter your Zip Code" />
-          <ButtonColor animated>
+          <Form.Checkbox
+            inline
+            label="Florida Resident"
+            onClick={handleClick}
+            checked={checked}
+          />
+          <Form.Checkbox
+            inline
+            label="Current Epi-Pen User"
+            onClick={handleClick}
+            checked={checked}
+          />
+          <Form.Checkbox
+            inline
+            label="Front Line Medical Worker"
+            onClick={handleClick}
+            checked={checked}
+          />
+          <Form.Checkbox
+            inline
+            label="Are you seeking your first dose?"
+            onClick={handleClick}
+            checked={checked}
+          />
+          <Form.Input
+            label="Zip Code"
+            placeholder="Enter your Zip Code"
+            value={zipCode}
+            onChange={(event) => setZipCode(event.target.value)}
+          />
+          <ButtonColor type="submit" animated>
             <StyledButtonText visible>REGISTER</StyledButtonText>
             <StyledButtonText hidden>
               <Icon name="arrow right" />
