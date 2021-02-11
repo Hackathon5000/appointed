55445db5fce423f7165133f7deb79baac55275fa
fabiolamariapina <fabiolamariapina@gmail.com>
2021-02-10 21:31:00 -0500

    after about two hours,i HAVE FINALLY aligned my form the way I can do it, it's not repsonsive but YOLO

--- src/components/Form/VaccineForm.js
+++ src/components/Form/VaccineForm.js
@@ -1,61 +1,63 @@
 import React, { useState } from "react";
-import { Form, Input, Button, Icon } from "semantic-ui-react";
+import { Form, Button, Icon } from "semantic-ui-react";
 import DatePicker from "react-datepicker";
+import styled from "styled-components";
 
 export default function VaccineForm() {
   const [selectedDate, setSelectedDate] = useState(null);
+  const MoveToCenter = styled(Form.Input)`
+    position: relative;
+    left: 400px;
+  `;
+  const AlignedResident = styled(Form.Checkbox)`
+    position: relative;
+    right: 16px;
+  `;
+  const AlignedWorker = styled(Form.Checkbox)`
+    position: relative;
+    left: 16px;
+  `;
+  const AlignedDose = styled(Form.Checkbox)`
+    position: relative;
+    left: 31px;
+  `;
+  const AlignZipCode = styled(Form.Input)`
+    position: relative;
+    left: 525px;
+  `;
   return (
     <div>
       <h1>Pre-Register Now For The COVID-19 Vaccine</h1>
-      <p>
-        For more information and updates regarding the COVID-19 vaccine in
-        Florida and to be notified when new appointments are available in your
-        area, please submit your email and additional information below.
-      </p>
       <Form>
-        <Form.Field required>
-          <label> Full Name </label>{" "}
-          <Input
-            error="Please enter your full name"
-            fluid
-            placeholder="Full Name"
-          />
-        </Form.Field>{" "}
-        <Form.Field required>
-          <label> Email </label>{" "}
-          <Input error="Please enter your email" fluid placeholder="Email" />
-        </Form.Field>{" "}
-        <Form.Field required>
-          <label> Phone Number </label>{" "}
-          <Input
-            error="Please enter your phone number"
-            fluid
-            placeholder="Phone Number"
-          />
-        </Form.Field>{" "}
-        <Form.Field required>
+        <MoveToCenter
+          label="Name"
+          placeholder="Enter your full legal name"
+          width={6}
+        />
+        <MoveToCenter
+          label="Email"
+          placeholder="example@example.com"
+          width={6}
+        />
+        <MoveToCenter label="Phone Number" placeholder="1234567890" width={6} />
+        <Form.Field>
           <label> Date of Birth </label>{" "}
           <DatePicker
             selected={selectedDate}
             onChange={(date) => setSelectedDate(date)}
-            error="Please enter your date of birth"
-            fluid
           />
         </Form.Field>{" "}
-        <Form.Checkbox required label="Florida Resident" />
-        <Form.Checkbox required label="Current Epi-Pen User" />
-        <Form.Checkbox required label="Front Line Medical Worker" />
-        <Form.Checkbox required label="Are you seeking your first dose?" />
-        <Form.Field required>
-          <label> Zipcode </label>{" "}
-          <Input
-            error="Please enter your phone number"
-            fluid
-            placeholder="Zip Code"
-          />
-        </Form.Field>
+        <AlignedResident inline label="Florida Resident" />
+        <Form.Checkbox inline label="Current Epi-Pen User" />
+        <AlignedWorker inline label="Front Line Medical Worker" />
+        <AlignedDose inline label="Are you seeking your first dose?" />
+        <AlignZipCode
+          label="Zip Code"
+          placeholder="Enter your Zip Code"
+          width={3}
+        />
         <Button animated>
-          <Button.Content visible>GET IN LINE</Button.Content>
+          <Button.Content visible>REGISTER</Button.Content>
           <Button.Content hidden>
             <Icon name="arrow right" />
           </Button.Content>
