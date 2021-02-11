714f2d1e3efe68d031849947c7f14e29cbf35326
fabiolamariapina <fabiolamariapina@gmail.com>
2021-02-11 10:07:00 -0500

    finall figured out flexbox wtih semantic ui, form is now centered

--- src/components/form/vaccineForm.jsx
+++ src/components/form/vaccineForm.jsx
@@ -5,25 +5,9 @@ import styled from "styled-components";
 
 export default function VaccineForm() {
   const [selectedDate, setSelectedDate] = useState(null);
-  const MoveToCenter = styled(Form.Input)`
-    position: relative;
-    left: 400px;
-  `;
-  const AlignedResident = styled(Form.Checkbox)`
-    position: relative;
-    right: 16px;
-  `;
-  const AlignedWorker = styled(Form.Checkbox)`
-    position: relative;
-    left: 16px;
-  `;
-  const AlignedDose = styled(Form.Checkbox)`
-    position: relative;
-    left: 31px;
-  `;
-  const AlignZipCode = styled(Form.Input)`
-    position: relative;
-    left: 525px;
+  const ParentDiv = styled.div`
+    display: flex;
+    justify-content: center;
   `;
   const ButtonColor = styled(Button)`
     &&& {
@@ -36,41 +20,31 @@ export default function VaccineForm() {
   return (
     <div>
       <h1>Pre-Register Now For The COVID-19 Vaccine</h1>
-      <Form>
-        <MoveToCenter
-          label="Name"
-          placeholder="Enter your full legal name"
-          width={6}
-        />
-        <MoveToCenter
-          label="Email"
-          placeholder="example@example.com"
-          width={6}
-        />
-        <MoveToCenter label="Phone Number" placeholder="1234567890" width={6} />
-        <Form.Field>
-          <label> Date of Birth </label>{" "}
-          <DatePicker
-            selected={selectedDate}
-            onChange={(date) => setSelectedDate(date)}
-          />
-        </Form.Field>{" "}
-        <AlignedResident inline label="Florida Resident" />
-        <Form.Checkbox inline label="Current Epi-Pen User" />
-        <AlignedWorker inline label="Front Line Medical Worker" />
-        <AlignedDose inline label="Are you seeking your first dose?" />
-        <AlignZipCode
-          label="Zip Code"
-          placeholder="Enter your Zip Code"
-          width={3}
-        />
-        <ButtonColor animated>
-          <StyledButtonText visible>REGISTER</StyledButtonText>
-          <StyledButtonText hidden>
-            <Icon name="arrow right" />
-          </StyledButtonText>
-        </ButtonColor>
-      </Form>{" "}
+      <ParentDiv>
+        <Form>
+          <Form.Input label="Name" placeholder="Enter your full legal name" />
+          <Form.Input label="Email" placeholder="example@example.com" />
+          <Form.Input label="Phone Number" placeholder="1234567890" />
+          <Form.Field>
+            <label> Date of Birth </label>{" "}
+            <DatePicker
+              selected={selectedDate}
+              onChange={(date) => setSelectedDate(date)}
+            />
+          </Form.Field>{" "}
+          <Form.Checkbox inline label="Florida Resident" />
+          <Form.Checkbox inline label="Current Epi-Pen User" />
+          <Form.Checkbox inline label="Front Line Medical Worker" />
+          <Form.Checkbox inline label="Are you seeking your first dose?" />
+          <Form.Input label="Zip Code" placeholder="Enter your Zip Code" />
+          <ButtonColor animated>
+            <StyledButtonText visible>REGISTER</StyledButtonText>
+            <StyledButtonText hidden>
+              <Icon name="arrow right" />
+            </StyledButtonText>
+          </ButtonColor>
+        </Form>{" "}
+      </ParentDiv>
     </div>
   );
 }
